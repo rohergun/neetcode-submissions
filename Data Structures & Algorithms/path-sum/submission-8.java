0bfc7f51ae07a5b1fe4846746b95481c10f59740
Rohat Ergun <123872606+rohergun@users.noreply.github.com>
2026-06-08 10:45:55 +0200

    Add: path-sum - submission-8

---
+++ Data Structures & Algorithms/path-sum/submission-8.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> sumStack = new Stack<>();
+        stack.push(root);
+        sumStack.push(targetSum - root.val);
+
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            int currSum = sumStack.pop();
+
+            if (node.left == null && node.right == null && currSum == 0){
+                return true;
+            }
+
+            if (node.right != null){
+                stack.push(node.right);
+                sumStack.push(currSum - node.right.val);
+            }
+
+            if (node.left != null){
+                stack.push(node.left);
+                sumStack.push(currSum - node.left.val);
+            }
+        }
+        return false;
+    }
+}
